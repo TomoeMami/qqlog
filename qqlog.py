0a377b2b3fd0f1e83912a226749957b8989ade02
TomoeMami <trembleafterme@outlook.com>
2021-09-23 18:00:01 +0800

    上传于 2021年09月23日 18:00

--- qqlog.py
+++ qqlog.py
@@ -96,7 +96,7 @@ class RequestHandler(BaseHTTPRequestHandler):
                         self.wfile.write(json.dumps(body).encode('utf-8'))
         elif json_obj['type'] == 'BotInvitedJoinGroupRequestEvent':
             print(json_obj)
-            if json_obj['fromId'] = '1747222904':
+            if json_obj['fromId'] == '1747222904':
                 self.send_response(200)
                 self.send_header("Content-type", "application/json")
                 self.end_headers()
