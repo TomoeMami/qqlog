1fa1b528cea415f0ab9e6535d1ea0bcc4a99789d
TomoeMami <trembleafterme@outlook.com>
2021-09-23 12:50:54 +0800

    上传于 2021年09月23日 12:50

--- bot.py
+++ bot.py
@@ -3,17 +3,21 @@ from urllib.parse import unquote
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class RequestHandler(BaseHTTPRequestHandler):
+    def handler(self):
+        print("data:", self.rfile.readline().decode())
+        self.wfile.write(self.rfile.readline())
+    
     def do_POST(self):
-        global dailydict
+        print(self.headers)
+        print(self.command)
         data = self.rfile.read(int(self.headers['content-length']))
-        data = unquote(str(data, encoding='utf-8'))
-        json_obj = json.loads(data)
+        # data = unquote(str(data, encoding='utf-8'))
+        # json_obj = json.loads(data)
+        json_obj = data.decode()
         print(json_obj)
         self.send_response(200)
         self.end_headers()
 if __name__ == "__main__":
-    global dailydict
-    dailydict = []
     addr = ('', 2334)
     server = HTTPServer(addr, RequestHandler)
     server.serve_forever()
