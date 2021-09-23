33c35486e45e5969d3a6666e04764b2cddefa378
TomoeMami <trembleafterme@outlook.com>
2021-09-23 14:55:26 +0800

    上传于 2021年09月23日 14:55

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from mirai import Mirai, WebSocketAdapter, FriendMessage
-from mirai import Event
+from mirai import BotInvitedJoinGroupRequestEvent
 if __name__ == '__main__':
     bot = Mirai(
         qq=3337523821, # 改成你的机器人的 QQ 号
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     def on_friend_message(event: FriendMessage):
         if str(event.message_chain) == '你好':
             return bot.send(event, 'Hello, World!')
-    @bot.on(Event)
-    def on_group_invited(event: Event):
-        print(str(Event))
+    @bot.on(BotInvitedJoinGroupRequestEvent)
+    def on_group_invited(event: BotInvitedJoinGroupRequestEvent):
+        if str(event.from_id) == '1747222904':
+            print(str(event.group_name))
     bot.run()
