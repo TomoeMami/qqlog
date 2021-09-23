49b06ae23b90e3577793bdebb62f90dfafa07f50
TomoeMami <trembleafterme@outlook.com>
2021-09-23 15:52:38 +0800

    上传于 2021年09月23日 15:52

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     def on_friend_message(event: FriendMessage):
         if str(event.message_chain) == '你好':
             return bot.send(event, 'Hello, World!')
-            
+
     @bot.on(BotInvitedJoinGroupRequestEvent)
     def on_group_invited(event: BotInvitedJoinGroupRequestEvent):
         if str(event.from_id) == '1747222904':
@@ -24,8 +24,10 @@ if __name__ == '__main__':
         print(str(event.nick))
         print(str(event.message))
         blocklist = ['']
-        if(event.from_id in blocklist):
-            return bot.decline(event, '您已被加入黑名单')
-        else:
-            return bot.allow(event)
+        # if(str(event.from_id) in blocklist):
+        #     return bot.decline(event, '您已被加入黑名单')
+        # else:
+        #     return bot.allow(event)
+
+
     bot.run()
